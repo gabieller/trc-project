48fcf815d7e707556a99a153e3a13caa0214c0be
Gabriela Eller <gabieller@hotmail.com>
2020-11-30 14:34:02 +0100

    Loading Page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,22 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Helmet } from "react-helmet"
 
 import Header from "./Header"
 import Footer from "./Footer"
 
 import "../styles/layout.css"
+import loadingGif from "../images/loading-unscreen.gif"
 
 export default function Layout(props) {
   const [isOpenSidebar, setOpenSidebar] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setInterval(() => {
+      setLoading(false)
+    }, 500)
+  }, [])
+
   return (
     <>
       <Helmet>
@@ -16,15 +25,26 @@ export default function Layout(props) {
         <link rel="canonical" href="https://theroutingcompany.com/" />
       </Helmet>
 
-      <Header uri={props.uri} onOpen={isOpen => setOpenSidebar(isOpen)} />
-      <div
-        className={`container-fluid main-container ${
-          isOpenSidebar ? "open" : ""
-        }`}
-      >
-        {props.children}
+    {loading &&(
+      <div className="d-flex justify-content-center loadingGif">
+        <img className="align-middle" src={loadingGif} alt="Loading Gif" width="150" height="150"/>
       </div>
-      <Footer />
+    )}
+    
+
+      {!loading && (
+        <>
+          <Header uri={props.uri} onOpen={isOpen => setOpenSidebar(isOpen)} />
+          <div
+            className={`container-fluid main-container ${
+              isOpenSidebar ? "open" : ""
+            }`}
+          >
+            {props.children}
+          </div>
+          <Footer />
+        </>
+      )}
     </>
   )
 }
