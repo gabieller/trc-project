edfe3759196691c9f191f52c7a0c186319825ea5
Gabriela Eller <gabieller@hotmail.com>
2020-12-04 18:08:13 +0100

    Side Menu links

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -4,6 +4,8 @@ import { Link } from "gatsby"
 import "../styles/sidemenu.css"
 
 import close from "../images/close-btn.svg"
+import linkedin from "../images/linkedin.svg"
+import twitter from "../images/twitter.svg"
 
 export default function SideMenu({ onClick, uri, visible }) {
   return (
@@ -17,38 +19,79 @@ export default function SideMenu({ onClick, uri, visible }) {
           >
             <img src={close} alt="Close button" width="30" height="30" />
           </button>
+          <div className="row container">
+            <div className="col-md-6 menuLink">
+              <div id="side-menu" className="menuLink col-md-6">
+                <ul className="navbar-nav ml-auto font-weight-bold">
+                  <li className={`nav-item ${uri === "/" ? "active" : ""}`}>
+                    <Link to="/" className="nav-link">
+                      Home
+                    </Link>
+                  </li>
+                  <li
+                    className={`nav-item ${uri === "/about" ? "active" : ""}`}
+                  >
+                    <Link to="/about" className="nav-link">
+                      About
+                    </Link>
+                  </li>
+                  <li
+                    className={`nav-item ${uri === "/pingo" ? "active" : ""}`}
+                  >
+                    <Link to="/pingo" className="nav-link">
+                      Pingo
+                    </Link>
+                  </li>
+                  <li
+                    className={`nav-item ${uri === "/product" ? "active" : ""}`}
+                  >
+                    <Link to="/solutions" className="nav-link">
+                      Solutions
+                    </Link>
+                  </li>
+                  <li
+                    className={`nav-item ${uri === "/contact" ? "active" : ""}`}
+                  >
+                    <Link to="/contact" className="nav-link">
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
 
-          <div id="side-menu" className="menuLink ">
-            <ul className="navbar-nav ml-auto font-weight-bold">
-              <li className={`nav-item ${uri === "/" ? "active" : ""}`}>
-                <Link to="/" className="nav-link">
-                  Home
-                </Link>
-              </li>
-              <li className={`nav-item ${uri === "/about" ? "active" : ""}`}>
-                <Link to="/about" className="nav-link">
-                  About
-                </Link>
-              </li>
-              <li className={`nav-item ${uri === "/pingo" ? "active" : ""}`}>
-                <Link to="/pingo" className="nav-link">
-                  Pingo
-                </Link>
-              </li>
-              <li className={`nav-item ${uri === "/product" ? "active" : ""}`}>
-                <Link to="/solutions" className="nav-link">
-                  Solutions
-                </Link>
-              </li>
-              <li className={`nav-item ${uri === "/contact" ? "active" : ""}`}>
-                <Link to="/contact" className="nav-link">
-                  Contact
-                </Link>
-              </li>
-            </ul>
+            <div className="col-md-6 contact">
+            <div className="col-md-6 pt-3 email-link">
+              <a href="mailto:info@theroutingcompany.com?subject=Press%20%26%20interviews">
+              info@theroutingcompany.com
+              </a>
+              </div>
+            <div className="col-md-6 pt-3 pl-0 social-media">
+              <button className={`btn btn-link`}>
+                <img
+                  src={linkedin}
+                  alt="Linkedin button"
+                  width="20"
+                  height="20"
+                />
+              </button>
+              <button className={`btn btn-link`}>
+                <img
+                  src={twitter}
+                  alt="Twitter button"
+                  width="20"
+                  height="20"
+                />
+              </button>
+            </div>
+          </div>
           </div>
         </div>
       </div>
     </>
   )
+}
+
+{
+  /* // info@theroutingcompany.com */
 }
