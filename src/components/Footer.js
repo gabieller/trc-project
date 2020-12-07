80135427e9ab61ef29f418858b6a80f8905710ff
Gabriela Eller <gabieller@hotmail.com>
2020-12-07 13:51:35 +0100

    Footer Alterations

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,61 @@ import Button from "@material-ui/core/Button"
 
 import "../styles/footer.css"
 
-import trclogo from "../images/trc-logo-black.png"
+import trclogo from "../images/trc-logo-white.png"
+import pingologo from "../images/pingo-logo-white.png"
+import linkedin from "../images/linkedin.svg"
+import twitter from "../images/twitter.svg"
+import googleplaybadge from "../images/google-play-badge.png"
+import appstorebadge from "../images/app-store-badge.svg"
 
 export default function Footer() {
   return (
     <footer id="footer" className="font-weight-bold px-5">
-      <div className="row container ">
-        <div className="col-md-4 logo pt-3">
-          <img src={trclogo} alt="Logo" width="234" height="40" />
+      <div className="row container">
+        <div className="d-flex flex-column col-md-4 logo-trc pt-3">
+          <div className="d-flex flex-column logo-trc ">
+            <img src={trclogo} alt="Logo TRC" width="70" height="63" />
+            <div className="social-media pb-3">
+              <button className={`btn btn-link`}>
+                <img
+                  src={twitter}
+                  alt="Twitter button"
+                  width="20"
+                  height="20"
+                />
+              </button>
+              <button className={`btn btn-link`}>
+                <img
+                  src={linkedin}
+                  alt="Linkedin button"
+                  width="20"
+                  height="20"
+                />
+              </button>
+            </div>
+          </div>
+
+          <div className="logo-pingo">
+            <img src={pingologo} alt="Logo PINGO" width="120" height="55" />
+            <div className="download-button pb-3">
+              <button className={`btn btn-link pl-2`}>
+                <img
+                  src={googleplaybadge}
+                  alt="Google Play Download"
+                  width="130"
+                  height="60"
+                />
+              </button>
+              <button className={`btn btn-link`}>
+                <img
+                  src={appstorebadge}
+                  alt="App Store Download"
+                  width="115"
+                  height="50"
+                />
+              </button>
+            </div>
+          </div>
         </div>
 
         <div className="col-md-4">
@@ -40,7 +87,7 @@ export default function Footer() {
               aria-label="| Enter your email"
             />
             <div className="input-group-append">
-              <Button className="btn-black" variant="contained">
+              <Button className="btn-white" variant="contained">
                 Subscribe
               </Button>
             </div>
@@ -48,9 +95,10 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="row container copyright pt-3 ">
-        <div className="col-md-12 ">
-          ©2020 The Routing Company — All Rights Reserved
+      <div className="row container copyright pt-3 pl-4">
+        <div className="d-flex flex-column col-md-3 pl-4">
+          <div>©2020 The Routing Company</div>
+          <div>All Rights Reserved</div>
         </div>
       </div>
     </footer>
