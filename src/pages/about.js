de74f37c88303d2285cf3378d9fe42910a3a0237
Gabriela Eller <gabieller@hotmail.com>
2020-12-07 17:30:38 +0100

    Changes in About Page Layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,9 @@ import React from "react"
 import Layout from "../components/Layout"
 import Button from "@material-ui/core/Button"
 
-import cardImage from "../images/card-example.png"
-import quote from "../images/quote.svg"
 import empty from "../images/img-empty.png"
-import icon from "../images/icon.svg"
+import menno from "../images/menno-pic.png"
+import arrow from "../images/arrow.svg"
 
 import "../styles/about.css"
 
@@ -31,126 +30,132 @@ export default function About(props) {
           </div>
         </div>
 
-        <div id="about-page-second" className="about-card">
-          <div className="row container justify-content-center">
-            <div className="card  bg-transparent m-5">
-              <img className="card-img-top" src={cardImage} alt="Card  cap" />
-              <div className="card-body pl-0">
-                <h4 className="card-title">Periculis Omittan</h4>
-                <p className="card-text">
-                  Est tation latine aliquip id, mea ad tale illud definitiones.
-                </p>
-                <img src={icon} alt="icon" width="36" height="36" />
+        <div id="about-page-second" className="row about-line">
+          <div className="col-md-5 border border-dark border-top border-bottom p-0 col-left ">
+            <div className="d-inline-flex">
+              <div className="col-md-6 team-picture py-5">
+                <img src={menno} alt="Menno Picture" width="250" height="250" />
+              </div>
+              <div className="card col-md-6 team-description bg-white ">
+                <div className="card-body">
+                  <h4 className="card-title font-weight-bold pt-5">
+                    Menno van der Zee
+                  </h4>
+                  <h5 className="card-subtitle mb-2 pb-3">Co-Founder</h5>
+                  <p className="card-text">
+                    Specialized in mobility-on-demand systems for high capacity
+                    vehicles.
+                  </p>
+                </div>
               </div>
             </div>
-            <div className="card  bg-transparent m-5">
-              <img className="card-img-top" src={cardImage} alt="Card  cap" />
-              <div className="card-body pl-0">
-                <h4 className="card-title">Periculis Omittan</h4>
-                <p className="card-text">
-                  Est tation latine aliquip id, mea ad tale illud definitiones.
-                </p>
-                <img src={icon} alt="icon" width="36" height="36" />
+
+            <div className="d-inline-flex border border-dark border-right-0 border-left-0">
+              <div className="col-md-6 team-picture py-5">
+                <img src={menno} alt="Menno Picture" width="250" height="250" />
+              </div>
+              <div className="card col-md-6 team-description bg-white ">
+                <div className="card-body">
+                  <h4 className="card-title font-weight-bold pt-5">
+                    Menno van der Zee
+                  </h4>
+                  <h5 className="card-subtitle mb-2 pb-3">Co-Founder</h5>
+                  <p className="card-text">
+                    Specialized in mobility-on-demand systems for high capacity
+                    vehicles.
+                  </p>
+                </div>
               </div>
             </div>
-            <div className="card bg-transparent m-5">
-              <img className="card-img-top" src={cardImage} alt="Card  cap" />
-              <div className="card-body pl-0">
-                <h4 className="card-title">Periculis Omittan</h4>
-                <p className="card-text">
-                  Est tation latine aliquip id, mea ad tale illud definitiones.
-                </p>
-                <img src={icon} alt="icon" width="36" height="36" />
+            <div className="d-inline-flex">
+              <div className="col-md-6 team-picture py-5">
+                <img src={menno} alt="Menno Picture" width="250" height="250" />
+              </div>
+              <div className="card col-md-6 team-description bg-white ">
+                <div className="card-body">
+                  <h4 className="card-title font-weight-bold pt-5">
+                    Menno van der Zee
+                  </h4>
+                  <h5 className="card-subtitle mb-2 pb-3">Co-Founder</h5>
+                  <p className="card-text">
+                    Specialized in mobility-on-demand systems for high capacity
+                    vehicles.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-2 border border-dark border-top-0 border-bottom-0">
+            <div className="arrow-interaction text-center">
+            <img src={arrow} alt="Arrow Interaction" />
+            </div>
+          </div>
+
+          <div className="col-md-5 border-dark border-top border-bottom p-0 col-right">
+            <div className="d-inline-flex">
+              <div className="col-md-6 team-picture py-5">
+                <img src={menno} alt="Menno Picture" width="250" height="250" />
+              </div>
+              <div className="card col-md-6 team-description bg-white ">
+                <div className="card-body">
+                  <h4 className="card-title font-weight-bold pt-5">
+                    Menno van der Zee
+                  </h4>
+                  <h5 className="card-subtitle mb-2 pb-3">Co-Founder</h5>
+                  <p className="card-text">
+                    Specialized in mobility-on-demand systems for high capacity
+                    vehicles.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="d-inline-flex d-inline-flex border border-dark border-right-0 border-left-0">
+              <div className="col-md-6 team-picture py-5">
+                <img src={menno} alt="Menno Picture" width="250" height="250" />
+              </div>
+              <div className="card col-md-6 team-description bg-white ">
+                <div className="card-body">
+                  <h4 className="card-title font-weight-bold pt-5">
+                    Menno van der Zee
+                  </h4>
+                  <h5 className="card-subtitle mb-2 pb-3">Co-Founder</h5>
+                  <p className="card-text">
+                    Specialized in mobility-on-demand systems for high capacity
+                    vehicles.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="d-inline-flex">
+              <div className="col-md-6 team-picture py-5">
+                <img src={menno} alt="Menno Picture" width="250" height="250" />
+              </div>
+              <div className="card col-md-6 team-description bg-white ">
+                <div className="card-body">
+                  <h4 className="card-title font-weight-bold pt-5">
+                    Menno van der Zee
+                  </h4>
+                  <h5 className="card-subtitle mb-2 pb-3">Co-Founder</h5>
+                  <p className="card-text">
+                    Specialized in mobility-on-demand systems for high capacity
+                    vehicles.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
 
         <div id="about-page-third">
-          <div className="row container justify-content-center about-quotes p-5 col-md-5">
-            <div
-              id="carouselExampleIndicators"
-              className="carousel slide"
-              data-ride="carousel"
-            >
-              <ol className="carousel-indicators">
-                <li
-                  data-target="#carouselExampleIndicators"
-                  data-slide-to="0"
-                  className="active"
-                ></li>
-                <li
-                  data-target="#carouselExampleIndicators"
-                  data-slide-to="1"
-                ></li>
-                <li
-                  data-target="#carouselExampleIndicators"
-                  data-slide-to="2"
-                ></li>
-              </ol>
-              <div className="carousel-inner">
-                <div className="carousel-item active">
-                  <div className="card text-center bg-transparent m-5">
-                    <img
-                      className="card-img-top"
-                      src={quote}
-                      alt="Card cap"
-                      width="100"
-                      height="100"
-                    />
-                    <div className="card-body">
-                      <p className="card-text">
-                        Est tation latine aliquip id, mea ad tale illud
-                        definitiones. Periculis omittantur reformidans
-                        necessitatibus eum ad, pro eripuit minimum comprehensam
-                        ne, usu cu stet prompta.
-                      </p>
-                      <h4 className="card-title">Connie Robertson at Google</h4>
-                    </div>
-                  </div>
-                </div>
-                <div className="carousel-item">
-                  <div className="card text-center bg-transparent m-5">
-                    <img
-                      className="card-img-top"
-                      src={quote}
-                      alt="Card cap"
-                      width="100"
-                      height="100"
-                    />
-                    <div className="card-body">
-                      <p className="card-text">
-                        Est tation latine aliquip id, mea ad tale illud
-                        definitiones. Periculis omittantur reformidans
-                        necessitatibus eum ad, pro eripuit minimum comprehensam
-                        ne, usu cu stet prompta.
-                      </p>
-                      <h4 className="card-title">Connie Robertson at Google</h4>
-                    </div>
-                  </div>
-                </div>
-                <div className="carousel-item">
-                  <div className="card text-center bg-transparent m-5">
-                    <img
-                      className="card-img-top"
-                      src={quote}
-                      alt="Card cap"
-                      width="100"
-                      height="100"
-                    />
-                    <div className="card-body">
-                      <p className="card-text">
-                        Est tation latine aliquip id, mea ad tale illud
-                        definitiones. Periculis omittantur reformidans
-                        necessitatibus eum ad, pro eripuit minimum comprehensam
-                        ne, usu cu stet prompta.
-                      </p>
-                      <h4 className="card-title">Connie Robertson at Google</h4>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="row container ">
+            <h2 className="col-md-12 text-center">
+              We build smart, sustainable transit solutions that are more
+              reliable and accessible than other mobility options. Our MIT-born
+              routing technology has been hailed as a striking example of the
+              potential around ridesharing.
+            </h2>
           </div>
         </div>
 
@@ -193,6 +198,38 @@ export default function About(props) {
                 sit ea brute mediocritatem, eu sed aliquam scripserit
                 dissentiunt.
               </h5>
+            </div>
+          </div>
+        </div>
+        <div id="contact-us" className="pl-5">
+          <div className="row container contact-us-title pl-0">
+            <div className="col-md-6 justify-content-left contact-us-title pb-3">
+              <h2>Ad eos saepe lucilius, noster postulant philosophia.</h2>
+            </div>
+          </div>
+          <div className="row container contact-us-subtitle pl-0">
+            <div className="col-md-6 col-sm-12 justify-content-left">
+              <h4>
+                Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad,
+                nam no suscipit quaerendum. At nam minimum ponderum. Est audiam
+                animal molestiae te. Ex duo eripuit mentitum.
+              </h4>
+            </div>
+          </div>
+          <div className="row container contact-us-buttons pl-0">
+            <div className=" justify-content-left pt-3">
+              <div className="action-buttons row pl-3">
+                <div className="action-buttons pr-3">
+                  <Button className="btn-black btn-action" variant="contained">
+                    Primary Action
+                  </Button>
+                </div>
+                <div className="action-buttons">
+                  <Button className="btn-white btn-action" variant="contained">
+                    Secondary Action
+                  </Button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
