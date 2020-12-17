747636ae6fe36d7f1ff793baa43ba9e8d9d2eead
Gabriela Eller <gabieller@hotmail.com>
2020-12-17 19:27:00 +0100

    Side Menu button without box

--- src/components/NavBarPingo.js
+++ src/components/NavBarPingo.js
@@ -7,16 +7,24 @@ export default function NavBar() {
     <nav id="navbar-pingo" className="navbar col-md-12 justify-content-center">
       <ul nav nav-pill>
         <li className="one nav-item px-5">
-          <a className="item font-weight-bold" href="#pingo-content">ABOUT</a>
+          <a className="item font-weight-bold" href="#pingo-content">
+            ABOUT
+          </a>
         </li>
         <li className="two nav-item px-5">
-          <a className="item font-weight-bold" href="#pingo-about">WHO BUILD</a>
+          <a className="item font-weight-bold" href="#pingo-about">
+            WHO BUILD
+          </a>
         </li>
         <li className="three nav-item px-5">
-          <a className="item font-weight-bold" href="#pingo-content"> WHERE YOU FIND</a>
+          <a className="item font-weight-bold" href="#pingo-content">
+            WHERE YOU FIND
+          </a>
         </li>
         <li className="four nav-item px-5">
-          <a className="item font-weight-bold" href="#pingo-content">GET PINGO</a>
+          <a className="item font-weight-bold" href="#pingo-content">
+            GET PINGO
+          </a>
         </li>
         <hr />
       </ul>
