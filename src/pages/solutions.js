2152a0893612537eed6eaa60c7cd0d348ad6227d
Gabriela Eller <gabieller@hotmail.com>
2020-12-21 20:48:59 +0100

    Finishing Solutions Page Mobile

--- src/pages/solutions.js
+++ src/pages/solutions.js
@@ -28,42 +28,42 @@ export default function Solutions(props) {
       <div id="solutions-page" className="container-fluid">
         <div className="section title justify-content-left pl-md-5 pl-xs-0">
           <div className="row container solutions-header-title pl-5 mx-3">
-            <div className="col-md-6 col-xs-8">
+            <div className="col-md-7 col-xs-8 pt-5">
               <h2>How we can help</h2>
             </div>
           </div>
-          <div className="d-flex inline row container pl-5">
-            <div className="card bg-white col-11 col-md-4 col-lg-3 my-2 mx-2 border border-dark">
-              <div className="card-body">
-                <h5 className="card-title font-weight-bold pt-2">
+          <div className="row container py-3 pl-5">
+            <div className="card col-lg-3 flex-md-column border border-dark py-4 px-4 mb-3 mx-3">
+              <div className="card-body p-0">
+                <h4 className="card-title col-md-10 px-0 text-left font-weight-bold">
                   Demand-responsive transit
-                </h5>
-                <div className="card-text pt-2">
+                </h4>
+                <h6 className="card-text my-4">
                   Many transit authorities worldwide experimenting with on
-                  demand buses
-                </div>
+                  demand buses.
+                </h6>
               </div>
             </div>
-            <div className="card bg-white border border-dark col-11 col-md-4 col-lg-3 my-2 mx-2 border border-dark">
-              <div className="card-body">
-                <h5 className="card-title font-weight-bold pt-2 my-0">
-                  Ridesharing
-                </h5>
-                <h5 className="card-title font-weight-bold">companies</h5>
-                <div className="card-text pt-2">
+            
+            <div className="card col-lg-3 flex-md-column border border-dark py-4 px-4 mb-3 mx-3">
+              <div className="card-body p-0">
+                <h4 className="card-title col-md-10 px-0 text-left font-weight-bold pt-2">
+                  Ridesharing companies
+                </h4>
+                <h6 className="card-text my-4">
                   It’s not easy to build an efficient pooling product. Reduce
                   engineering costs.
-                </div>
+                </h6>
               </div>
             </div>
-            <div className="card bg-white border border-dark col-11 col-md-4 col-lg-3 my-2 mx-2 border border-dark">
-              <div className="card-body">
-                <h5 className="card-title font-weight-bold pt-2">
+            <div className="card col-lg-3 flex-md-column border border-dark py-4 px-4 mb-3 mx-3">
+              <div className="card-body p-0">
+                <h4 className="card-title col-md-11 px-0 text-left font-weight-bold">
                   Transit & para-transit (WAV) authorities
-                </h5>
-                <div className="card-text pt-2">
+                </h4>
+                <h6 className="card-text my-4">
                   Wait times are typically far higher for those who needs WAVs.
-                </div>
+                </h6>
               </div>
             </div>
           </div>
@@ -74,16 +74,16 @@ export default function Solutions(props) {
             <>
               <div id="solutions-how-it-works">
                 <div className="row p-0">
-                  <div className="col-md-7 border border-dark border-right-0 title">
+                  <div className="col-sm-7 border border-dark border-right-0 title">
                     <h2 className="justify-content-left">How it works</h2>
-                    <div className="col-md-8 p-0 p-sm-2">
+                    <p className="col-md-10 p-0 p-sm-2 pl-0">
                       We dynamically routes shared rides in real time through a
                       fleet management dashboard, a rider app and a driver app,
                       all powered by MIT-born technology. It’s easy to use and
                       built for speed at city-wide scale.
-                    </div>
+                    </p>
                   </div>
-                  <div className="col-md-5 p-0">
+                  <div className="col-sm-5 p-0">
                     <img
                       src={map}
                       alt="Map"
@@ -94,34 +94,34 @@ export default function Solutions(props) {
                   </div>
                 </div>
               </div>
+
               <div id="solutions-lines">
                 <div className="row">
-                  <div className="col-md-5 p-0 border-bottom border-dark col-right">
-                    <div>
+                  <div className="col-sm-5 p-0  col-right">
                       <img
                         src={solutionslinesimage}
+                        className="border-bottom border-dark"
                         alt="Solutions"
                         width="100%"
                         height="100%"
                       />
-                    </div>
                   </div>
 
-                  <div className="col-md-2 border-dark border-left arrow-interaction ">
+                  <div className="col-sm-2 border-dark border-left arrow-interaction ">
                     <div className="text-center">
                       <img src={arrow} alt="Arrow Interaction" />
                     </div>
                   </div>
 
-                  <div className="col-md-5 border-left border-bottom border-dark pt-5 col-left">
-                    <div className="row container py-5 col-md-8">
-                      <h3 className="pb-3">1. We partner with you</h3>
+                  <div className="col-sm-5 border-left border-bottom border-dark pt-5 col-left">
+                    <div className="row container py-5 col-md-9">
+                      <h3 className="pb-3 font-weight-bold">1. We partner with you</h3>
                       We work with you to build solutions that are tailor-made
                       to your transit needs.
                     </div>
                     <div className="row container ">
-                      <div className="row container py-5 col-md-8">
-                        <h3 className="pb-3">
+                      <div className="row container py-5 col-md-9">
+                        <h3 className="pb-3 font-weight-bold">
                           2. Our routing engine gets to work
                         </h3>
                         You get access to tools and technology that calculate
@@ -130,8 +130,8 @@ export default function Solutions(props) {
                       </div>
                     </div>
                     <div className="row container">
-                      <div className="row container py-5 col-md-8">
-                        <h3 className="pb-3">
+                      <div className="row container py-5 col-md-9">
+                        <h3 className="pb-3 font-weight-bold">
                           3. Less stress, more time and money saved
                         </h3>
                         Riders enjoy lower wait times and more reliable service.
@@ -141,24 +141,23 @@ export default function Solutions(props) {
                 </div>
               </div>
               <div id="solutions-products">
-                <div className="d-flex flex-row container pt-5">
-                  <div className="col-md-2 text-center">
+                <div className="d-flex flex-row container pt-5 justify-content-center">
+                  <div className="col-sm-3 text-center">
                     Exponentially richer possibility set
                   </div>
-                  <div className="col-md-3 text-center">
+                  <div className="col-sm-1 text-center">
                     <img src={add_circle} alt="Add Icon" />
                   </div>
-                  <div className="col-md-2 text-center">
+                  <div className="col-sm-3 text-center">
                     Global optimization
                   </div>
-                  <div className="col-md-3 text-center">
+                  <div className="col-sm-1 text-center">
                     <img src={add_circle} alt="Add Icon" />
                   </div>
-                  <div className="col-md-2 text-center">
+                  <div className="col-sm-3 text-center">
                     Rebalancing for future performance
                   </div>
                 </div>
-                {/* <hr className="line"/> */}
                 <div className="row container justify-content-center pt-4 equal-icon">
                   <img src={equal_circle} alt="Equal Icon" />
                 </div>
@@ -180,28 +179,28 @@ export default function Solutions(props) {
                   <div className="col-xs-3">
                     <h2 className="justify-content-left pb-3">How it works</h2>
                   </div>
-                  <div className="col-xs-3 pb-5">
+                  <p className="col-xs-3 pb-5">
                     We dynamically routes shared rides in real time through a
                     fleet management dashboard, a rider app and a driver app,
                     all powered by MIT-born technology. It’s easy to use and
                     built for speed at city-wide scale.
-                  </div>
+                  </p>
                 </div>
               </div>
               <div id="solutions-topics">
                 <div className="row border-bottom border-dark pt-5">
                   <div className="row container">
                     <div className="row container py-3 px-4">
-                      <h4 className="pb-2">1. We partner with you</h4>
+                      <h3 className="pb-2">1. We partner with you</h3>
                       We work with you to build solutions that are tailor-made
                       to your transit needs.
                     </div>
                   </div>
                   <div className="row container">
                     <div className="row container py-3 px-4">
-                      <h4 className="pb-2">
+                      <h3 className="pb-2">
                         2. Our routing engine gets to work
-                      </h4>
+                      </h3>
                       You get access to tools and technology that calculate the
                       most optimal routes for your vehiclís and rides in real
                       time.
@@ -209,9 +208,9 @@ export default function Solutions(props) {
                   </div>
                   <div className="row container">
                     <div className="row container pt-3 pb-5 px-4">
-                      <h4 className="pb-2">
+                      <h3 className="pb-2">
                         3. Less stress, more time and money saved
-                      </h4>
+                      </h3>
                       Riders enjoy lower wait times and more reliable service.
                     </div>
                   </div>
@@ -250,9 +249,9 @@ export default function Solutions(props) {
         </div>
 
         <div id="pingo-solutions">
-          <div className="row container py-5  px-5">
+          <div className="row container py-5 px-5">
             <div className="row border border-dark bg-white">
-              <div className="col-md-5 px-0">
+              <div className="col-sm-5 col-xs-3 px-0">
                 <img
                   src={pingosolutions}
                   alt="Pingo Solutions"
@@ -260,7 +259,7 @@ export default function Solutions(props) {
                   height="100%"
                 />
               </div>
-              <div className="col-md-7 py-md-5 py-xs-0">
+              <div className="col-sm-7 col-xs-8 py-md-5 py-xs-0">
                 <div className="row container justify-content-left">
                   <h2 className="font-weight-bold pt-5">Pingo</h2>
                 </div>
@@ -271,7 +270,7 @@ export default function Solutions(props) {
                   Et has minim elitr intellegat. Mea aeterno eleifend antiopam
                   ad, nam no suscipit quaerendum. At nam minimum ponderum. Est
                   audiam animal molestiae te. Ex duo eripuit mentitum.
-                  <div className="action-buttons px-0 py-3 col-md-4 col-sm-12">
+                  <div className="action-buttons px-0 py-3 col-sm-4 col-sm-12">
                     <Button className="btn-black" variant="contained">
                       Primary Action
                     </Button>
