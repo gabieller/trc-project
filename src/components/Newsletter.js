dbfe2d5ac8e20a42602307dbb3f6087875342ccc
Gabriela Eller <gabieller@hotmail.com>
2021-01-09 18:24:52 +0100

    Refactoring About page and Newsletter Component

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -20,40 +20,52 @@ function SubscribeForm() {
   const handleChange = event => setEmail(event.target.value)
 
   return (
-    <div className="row container col-12 col-md-9">
-      <form>
-        <div className="newsletter-title">
-          <h4>Follow our latest news</h4>
-          <h4 className="pb-4">Sign up for our newsletter</h4>
-          Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam
-          no suscipit quaerendum. At nam minimum ponderum. Est audiam animal
-          molestiae te. Ex duo eripuit mentitum.
-        </div>
+    <div className="section">
+      <div className="container no-gutters">
+        <div className="row col-12 col-md-10">
+          <form>
+            <h4>Follow our latest news</h4>
+            <h4 className="pb-5">Sign up for our newsletter</h4>
+            Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam
+            no suscipit quaerendum. At nam minimum ponderum. Est audiam animal
+            molestiae te. Ex duo eripuit mentitum.
+            <div className="d-column-flex d-md-inline-flex col-12 pt-3 px-0">
+              <input
+                className="col-12 col-md-8"
+                type="email"
+                onChange={handleChange}
+                value={email}
+                placeholder="| Enter your email"
+                required
+              />
 
-        <div className="d-column-flex d-md-inline-flex col-xl-12 pt-3 px-0">
-          <input
-            className="col-xl-8"
-            type="email"
-            onChange={handleChange}
-            value={email}
-            placeholder="| Enter your email"
-            required
-          />
-
-          <div className="input-group-append col-xl-12 p-0">
-            <Button
-              onClick={handleSubmit}
-              className="btn-black btn-block w-25"
-              variant="contained"
-            >
-              Subscribe
-            </Button>
+              <div className="input-group-append col-12 col-md-4 p-0">
+                <Button
+                  onClick={handleSubmit}
+                  className="btn-black btn-block"
+                  variant="contained"
+                >
+                  Subscribe
+                </Button>
+              </div>
+            </div>
+            <div className="d-flex flex-column pt-3">
+              <span status={status}>{message}</span>
+            </div>
+          </form>
+          <div className="row no-gutters">
+          <div className="col-md-10 warning-text">
+            <h6>
+              <small>
+                By clicking “Primary action” you accepting ipsum dolor sit amet,
+                sit ea brute mediocritatem, eu sed aliquam scripserit
+                dissentiunt.
+              </small>
+            </h6>
+          </div>
           </div>
         </div>
-        <div className="d-flex flex-column pt-3">
-          <span status={status}>{message}</span>
-        </div>
-      </form>
+      </div>
     </div>
   )
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,6 @@ import Layout from "../components/Layout"
 import Demo from "../components/Demo"
 import Arrow from "../components/Arrow"
 import Newsletter from "../components/Newsletter"
-import Responsive from "../components/Responsive"
 import CardTeam from "../components/CardTeam"
 
 import newsletterimage from "../images/newsletterimage.png"
@@ -15,30 +14,28 @@ import "../styles/about.css"
 export default function About(props) {
   return (
     <Layout {...props}>
-      <div id="about-page" className="container-fluid">
-        <div className="section title pl-xs-0">
-          <div className="row container pt-5">
-            <div className="col-xl-8 col-xs-8">
-              <h1>
-                A world-class team focused on solving transportation problems
-              </h1>
+      <div id="about" className="container-fluid">
+        <div className="section title">
+          <div className="container title-content">
+            <div className="row no-gutters">
+              <div className="col-12 col-md-8">
+                <h1>
+                  A world-class team focused on solving transportation problems
+                </h1>
+              </div>
             </div>
-          </div>
-          <div className="row container justify-content-left pt-2">
-            <div className="col-xl-7">
-              We are a team of builders from technology, transportation,
-              academia and public policy with a passion for helping cities
-              unlock transit’s full potential.
+            <div className="row no-gutters justify-content-left">
+              <div className="col-12 col-md-6">
+                We are a team of builders from technology, transportation,
+                academia and public policy with a passion for helping cities
+                unlock transit’s full potential.
+              </div>
             </div>
           </div>
         </div>
-
-        <Responsive.Desktop>
-          <div
-            id="about-team"
-            className="row about-line border-dark border-top"
-          >
-            <div className="col-md-5 border-dark  border-bottom p-0 col-left ">
+        <div className="section team-card">
+          <div className="row border-top border-dark no-gutters">
+            <div className="col-12 col-md-5 border-bottom border-dark col-left ">
               <CardTeam
                 imgsize="50"
                 memberpicture={menno}
@@ -80,7 +77,7 @@ export default function About(props) {
 
             <Arrow />
 
-            <div className="col-md-5 border-dark border-bottom p-0 col-right">
+            <div className="col-12 col-md-5 border-bottom border-dark col-right">
               <CardTeam
                 imgsize="50"
                 memberpicture={menno}
@@ -93,6 +90,7 @@ export default function About(props) {
                 companylogo={uber}
               />
               <hr />
+
               <CardTeam
                 imgsize="50"
                 memberpicture={menno}
@@ -105,6 +103,7 @@ export default function About(props) {
                 companylogo={uber}
               />
               <hr />
+
               <CardTeam
                 imgsize="50"
                 memberpicture={menno}
@@ -117,116 +116,33 @@ export default function About(props) {
                 companylogo={uber}
               />
             </div>
-          </div>
-        </Responsive.Desktop>
-
-        <Responsive.Mobile>
-          <div
-            id="about-team-mobile"
-            className="row container border-top border-bottom border-dark"
-          >
-            <CardTeam
-              imgsize="75"
-              memberpicture={menno}
-              linkedinurl={"https://www.linkedin.com/in/mennovanderzee/"}
-              name={"Menno van der Zee"}
-              position={"Co-Funder"}
-              description={
-                "Specialized in mobility-on-demand systems for high capacity vehicles."
-              }
-              companylogo={uber}
-            />
-
-            <CardTeam
-              imgsize="75"
-              memberpicture={menno}
-              linkedinurl={"https://www.linkedin.com/in/mennovanderzee/"}
-              name={"Menno van der Zee"}
-              position={"Co-Funder"}
-              description={
-                "Specialized in mobility-on-demand systems for high capacity vehicles."
-              }
-              companylogo={uber}
-            />
-
-            <CardTeam
-              imgsize="75"
-              memberpicture={menno}
-              linkedinurl={"https://www.linkedin.com/in/mennovanderzee/"}
-              name={"Menno van der Zee"}
-              position={"Co-Funder"}
-              description={
-                "Specialized in mobility-on-demand systems for high capacity vehicles."
-              }
-              companylogo={uber}
-            />
-            <CardTeam
-              imgsize="75"
-              memberpicture={menno}
-              linkedinurl={"https://www.linkedin.com/in/mennovanderzee/"}
-              name={"Menno van der Zee"}
-              position={"Co-Funder"}
-              description={
-                "Specialized in mobility-on-demand systems for high capacity vehicles."
-              }
-              companylogo={uber}
-            />
-            <CardTeam
-              imgsize="75"
-              memberpicture={menno}
-              linkedinurl={"https://www.linkedin.com/in/mennovanderzee/"}
-              name={"Menno van der Zee"}
-              position={"Co-Funder"}
-              description={
-                "Specialized in mobility-on-demand systems for high capacity vehicles."
-              }
-              companylogo={uber}
-            />
-
-            <CardTeam
-              imgsize="75"
-              memberpicture={menno}
-              linkedinurl={"https://www.linkedin.com/in/mennovanderzee/"}
-              name={"Menno van der Zee"}
-              position={"Co-Funder"}
-              description={
-                "Specialized in mobility-on-demand systems for high capacity vehicles."
-              }
-              companylogo={uber}
-            />
-          </div>
-        </Responsive.Mobile>
-
-        <div className="row container justify-content-center pt-5">
-          <div className="col-10 col-md-6 text-center pb-5">
-            We build smart, sustainable transit solutions that are more reliable
-            and accessible than other mobility options. Our MIT-born routing
-            technology has been hailed as a striking example of the potential
-            around ridesharing.
           </div>
         </div>
-
-        <div id="newsletter-section" className="row px-5">
-          <div className="row container bg-white border border-dark px-0">
-            <div className="col-md-5 px-0">
-              <img
-                src={newsletterimage}
-                alt="Newsletter"
-                className="img-fluid"
-                width="100%"
-              />
+        <div className="section">
+          <div className="container ">
+            <div className="row justify-content-center no-gutters">
+              <div className="col-10 col-md-6 text-center">
+                We build smart, sustainable transit solutions that are more
+                reliable and accessible than other mobility options. Our
+                MIT-born routing technology has been hailed as a striking
+                example of the potential around ridesharing.
+              </div>
             </div>
-            <div className="col-md-7 border border-dark  newsletter-box">
-              <Newsletter />
-
-              <div className="row container col-md-9 warning-text">
-                <h6>
-                  <small>
-                    By clicking “Primary action” you accepting ipsum dolor sit
-                    amet, sit ea brute mediocritatem, eu sed aliquam scripserit
-                    dissentiunt.
-                  </small>
-                </h6>
+          </div>
+        </div>
+        <div className="section newsletter">
+          <div className="container">
+            <div className="row  border border-dark no-gutters bg-white">
+              <div className="col-12 col-md-5">
+                <img
+                  src={newsletterimage}
+                  alt="Newsletter"
+                  className="img-fluid"
+                  width="100%"
+                />
+              </div>
+              <div className="col-12 col-md-7">
+                <Newsletter />
               </div>
             </div>
           </div>
