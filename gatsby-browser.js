226a49b98b1a1bbef46e90ed41790f7d8c1cc556
Gabriela Eller <gabieller@hotmail.com>
2021-01-13 15:43:27 +0100

    Remove jquery

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/js/bootstrap.min.js"
 import "bootstrap/dist/css/bootstrap.css"
-import "jquery/dist/jquery.min.js"
 import "popper.js/dist/popper.min"
 import "./src/styles/global.css"
+
