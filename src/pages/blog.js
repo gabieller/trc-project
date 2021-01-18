9902c9ce1a0cdd588f0a356059009d805d667697
Gabriela Eller <gabieller@hotmail.com>
2021-01-18 18:22:56 +0100

    Background color global

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react"
 import Layout from "../components/Layout"
 import Demo from "../components/Demo"
 import Responsive from "../components/Responsive"
-import "../styles/blog.css"
 
 function widgetDownload(src) {
   return new Promise(function (resolve, reject) {
@@ -19,9 +18,6 @@ function widgetDownload(src) {
 }
 
 var my_script = widgetDownload("https://medium-widget.pixelpoint.io/widget.js")
-// var my_script_mobile = widgetDownload(
-//   "https://medium-widget.pixelpoint.io/widget.js"
-// )
 
 export default function (props) {
   useEffect(() => {
