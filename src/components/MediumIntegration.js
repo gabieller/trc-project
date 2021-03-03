070dec5f1be9872456a7acd6156e5b1ce400a494
Jhonata da Rocha <jhonata11@hotmail.com>
2021-03-03 17:02:27 -0300

    change medium feed to rss

--- src/components/MediumIntegration.js
+++ src/components/MediumIntegration.js
@@ -1,35 +1,65 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 
 export default function () {
-  const hasInit = typeof window.MediumWidget.Init === "function"
-  const hasUnmout = typeof window.MediumWidget.unmount === "function"
-  if (!hasInit || !hasUnmout) {
-    return null
-  }
+  const [posts, setPosts] = useState([])
 
   useEffect(() => {
-    window.MediumWidget.Init({
-      renderTo: "#medium-widget",
-      params: {
-        resource: "https://medium.com/routable-ai",
-        postsPerLine: 2,
-        limit: 4,
-        picture: "big",
-        fields: ["description", "author", "claps", "publishAt"],
-        ratio: "square",
-      },
-    })
-
-    return () => {
-      window.MediumWidget.unmount()
+    async function getMediumFeed() {
+      try {
+        const mediumRssFeed = "https://medium.com/feed/routable-ai"
+        const rssToJsonApi = "https://api.rss2json.com/v1/api.json"
+        const data = { params: { rss_url: mediumRssFeed } }
+        const res = await axios.get(rssToJsonApi, data)
+        if (res.data.status === "ok") {
+          setPosts(res.data.items)
+        }
+      } catch (error) {
+        console.error(error)
+      }
     }
+    getMediumFeed()
   }, [])
 
+  const sortedPosts = posts.sort((a, b) => (a.pubDate < b.pubDate ? 1 : -1))
+  console.log(sortedPosts.map(e => e.thumbnail))
   return (
-    <div id="medium" className="container-fluid">
-      <div className="container">
-        <div id="medium-widget" />
-      </div>
+    <div className="row row-cols-1 row-cols-md-2 g-4">
+      {sortedPosts.map(e => (
+        <div key={e.title} className="col">
+          <div className="card h-100">
+            <img
+              // src={"https://cdn-images-1.medium.com/max/1024/1*OZcyztxLDRSjrCWRRIUphA.png"}
+              src={e.thumbnail}
+              className="card-img-top"
+              alt={e.title}
+              style={{
+                height: "25vw",
+                objectFit: "cover",
+              }}
+            />
+            <div className="card-body">
+              <h5 className="card-title">{e.title}</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+            <div className="card-footer bg-transparent">
+              <span className="card-text">
+                {e.author}
+                {" - "}
+                <span className="text-muted">
+                  {new Date(e.pubDate).toLocaleString("default", {
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
