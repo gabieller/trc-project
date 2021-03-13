6bbbed575548f2c1a2794a888277c834871f4724
Jhonata da Rocha <jhonata11@hotmail.com>
2021-03-13 22:55:52 +0100

    fix arrow animation

--- src/components/Arrow.js
+++ src/components/Arrow.js
@@ -2,49 +2,39 @@ import React, { useState, useRef } from "react"
 import { useEffectOnlyOnce } from "../utils/hooks"
 import "../styles/arrow.css"
 
-export default function Arrow({ className }) {
-  const [percentage, setPercentage] = useState(0)
-  const [showArrow, setShowArrow] = useState(false)
+export default function Arrow() {
+  const [arrowHeight, setArrowHeight] = useState(0)
   const selectedDiv = useRef(null)
 
   useEffectOnlyOnce(() => {
     const onScroll = () => {
       if (selectedDiv.current) {
-        const { scrollTop } = document.documentElement
+        const { clientHeight } = document.documentElement
         const { scrollHeight, offsetTop } = selectedDiv.current
-        const bottomGoal = scrollHeight / 2 + offsetTop
-        const newValue = (scrollTop / bottomGoal) * 100
-        setPercentage(newValue >= 100 ? 100 : newValue)
+        const halfScreenOffset = window.pageYOffset + clientHeight / 2
+
+        let height = halfScreenOffset - offsetTop
+        height = height > 20 ? height : 0
+        height = height >= scrollHeight ? scrollHeight : height
+        setArrowHeight(height)
       }
     }
     window.addEventListener("scroll", onScroll)
     return () => window.removeEventListener("scroll", onScroll)
   })
 
-  useEffectOnlyOnce(() => {
-    const checkArrow = () => {
-      if (!showArrow && window.pageYOffset > 0) {
-        setShowArrow(true)
-      } else {
-        setShowArrow(false)
-      }
-    }
-    window.addEventListener("scroll", checkArrow)
-    return () => window.removeEventListener("scroll", checkArrow)
-  })
-
-  const arrowTip = `line ${showArrow ? "visible" : "invisible"}`
   return (
     <div
-      className="rolling-arrow col-md-2  justify-content-center d-flex"
+      className="rolling-arrow col-md-2 justify-content-center d-flex"
       ref={selectedDiv}
     >
-      <div
-        className="arrow2 d-none d-md-block"
-        style={{ height: `calc(0px + ${percentage}%)` }}
-      >
-        <span className={`line-1 ${arrowTip}`} />
-        <span className={`line-2 ${arrowTip}`} />
+      <div className="arrow2 d-none d-md-block" style={{ height: arrowHeight }}>
+        {arrowHeight > 20 && (
+          <>
+            <span className="line line-1" />
+            <span className="line line-2" />
+          </>
+        )}
       </div>
     </div>
   )
