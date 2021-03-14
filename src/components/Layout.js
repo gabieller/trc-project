fc54b41e4b9eb3e5933b81207bbfb030670aa3f5
Jhonata da Rocha <jhonata11@hotmail.com>
2021-03-14 19:14:29 +0100

    do loading only once

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,39 +1,16 @@
-import React, { useState, useEffect } from "react"
-
-import { Helmet } from "react-helmet"
+import React, { useState } from "react"
 
 import Header from "./Header"
 import CookieConsent from "../components/CookieConsent"
 import Footer from "./Footer"
 
 import "../styles/layout.css"
-// import loadingtrc from "../images/trc-loading.png"
-import loadingtrc from "../images/TRC_logo_motion.gif"
 
 export default function Layout(props) {
   const [isOpenSidebar, setOpenSidebar] = useState(false)
-  const [loading, setLoading] = useState(true)
   const isSSR = typeof window === "undefined"
-
-  useEffect(() => {
-    setInterval(() => {
-      setLoading(false)
-    }, 2000)
-  }, [])
-
   return (
     <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>The Routing Company</title>
-        <link rel="canonical" href="https://theroutingcompany.com/" />
-        <script src="https://medium-widget.pixelpoint.io/widget.js" />
-      </Helmet>
-
-      <div className={`loading ${loading ? "open" : ""}`}>
-        <img src={loadingtrc} alt="Loading Gif" width="1500" />
-      </div>
-
       {!isSSR && (
         <>
           <Header uri={props.uri} onOpen={isOpen => setOpenSidebar(isOpen)} />
