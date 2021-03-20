9024a2743c167db46c5aed8d3577b1604bb1f7a2
Jhonata da Rocha <jhonata11@hotmail.com>
2021-03-20 15:51:03 +0100

    fix animation

--- src/components/CanvasAnimation.js
+++ src/components/CanvasAnimation.js
@@ -20,41 +20,38 @@ const createLines = space => {
 
 export default function Canvas() {
   const [lines, setLines] = useState([])
-  const [space, setSpace] = useState(null)
 
   const onResize = useCallback(space => {
-    setSpace(space)
     setLines(createLines(space.bound))
   }, [])
 
   const onStart = useCallback((bound, space) => {
-    setSpace(space)
     setLines(createLines(bound))
   }, [])
 
-  const onAnimate = useCallback(() => {
-    const form = space.getForm()
-    console.log(form)
-    const range = Circle.fromCenter(space.pointer, 20)
-    form.stroke("#fff").lines(lines)
-    lines.forEach(line => {
-      const inPath = Circle.intersectRay2D(range, line)
-      const inLine = Circle.intersectLine2D(range, line)
+  const onAnimate = useCallback(
+    (space, form) => {
+      const range = Circle.fromCenter(space.pointer, 20)
+      form.stroke("#fff").lines(lines)
+      lines.forEach(line => {
+        const inPath = Circle.intersectRay2D(range, line)
+        const inLine = Circle.intersectLine2D(range, line)
+        if (inPath.length > 1) {
+          form
+            .stroke("rgba(44,84,53,.10)")
+            .line(line.concat(inPath[1], inPath[0]))
+          form.stroke("#BEC6C4").line(line)
+          form.fillOnly("#000").points(inPath, 6, "circle")
+        }
 
-      if (inPath.length > 1) {
-        form
-          .stroke("rgba(44,84,53,.10)")
-          .line(line.concat(inPath[1], inPath[0]))
-        form.stroke("#BEC6C4").line(line)
-        form.fillOnly("#000").points(inPath, 6, "circle")
-      }
-
-      if (inLine.length > 0) {
-        form.stroke("#000").line(line)
-        form.fillOnly("#000").points(inLine, 12, "circle")
-      }
-    })
-  }, [lines, space])
+        if (inLine.length > 0) {
+          form.stroke("#000").line(line)
+          form.fillOnly("#000").points(inLine, 12, "circle")
+        }
+      })
+    },
+    [lines]
+  )
 
   return (
     <QuickStartCanvas
